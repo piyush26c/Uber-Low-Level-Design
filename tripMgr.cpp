fe0ded0a7f838a797ce3525a6ae9e1be6b04b006
Piyush Chaudhari <piyushrc26@gmail.com>
2024-07-22 11:38:06 -0400

    added lock_guard and smart pointer implementation.

--- tripMgr.cpp
+++ tripMgr.cpp
@@ -1,36 +1,31 @@
 #include "tripMgr.hpp"
 
-TripMgr* TripMgr::tripMgrInstance = nullptr;
+shared_ptr<TripMgr> TripMgr::tripMgrInstance = nullptr;
 mutex TripMgr::mtx;
+atomic<int> TripMgr::nextTripId{1}; // Initialize atomic variable
 
-
-TripMgr* TripMgr::getTripMgr() {
-	if (tripMgrInstance == nullptr) {
-		mtx.lock();
-		if (tripMgrInstance == nullptr) {
-			tripMgrInstance = new TripMgr();
-		}
-		mtx.unlock();
-	}
-	return tripMgrInstance;
+shared_ptr<TripMgr> TripMgr::getTripMgr() {
+    lock_guard<mutex> lock(mtx);
+    if (!tripMgrInstance) {
+        tripMgrInstance = shared_ptr<TripMgr>(new TripMgr());
+    }
+    return tripMgrInstance;
 }
 
-void TripMgr::CreateTrip(Rider* pRider, Location* pSrcLoc, Location* pDstLoc) {
-	
-	TripMetaData* metaData = new TripMetaData(pSrcLoc, pDstLoc, pRider->getRating());
-	StrategyMgr* strategyMgr = StrategyMgr::getStrategyMgr();
-	PricingStrategy* pricingStrategy = strategyMgr->determinePricingStrategy(metaData);
-	DriverMatchingStrategy* driverMatchingStrategy = strategyMgr->determineMatchingStrategy(metaData);
-	
-	Driver* driver = driverMatchingStrategy->matchDriver(metaData);
-	double tripPrice = pricingStrategy->calculatePrice(metaData);
+void TripMgr::CreateTrip(shared_ptr<Rider> pRider, shared_ptr<Location> pSrcLoc, shared_ptr<Location> pDstLoc) {
+    int id = nextTripId++; // Increment the tripId in a thread-safe manner
+    auto metaData = make_shared<TripMetaData>(pSrcLoc, pDstLoc, pRider->getRating());
+    auto strategyMgr = StrategyMgr::getStrategyMgr();
+    auto pricingStrategy = strategyMgr->determinePricingStrategy(metaData);
+    auto driverMatchingStrategy = strategyMgr->determineMatchingStrategy(metaData);
+    auto driver = driverMatchingStrategy->matchDriver(metaData);
+    double tripPrice = pricingStrategy->calculatePrice(metaData);
 
-	Trip* trip = new Trip(pRider, driver, pSrcLoc, pDstLoc, tripPrice, pricingStrategy, driverMatchingStrategy);
-	int tripId = trip->getTripId();
-	tripsInfo[tripId] = trip;
-	tripsMetaDataInfo[tripId] = metaData;
+    auto trip = make_shared<Trip>(id, pRider, driver, pSrcLoc, pDstLoc, tripPrice, pricingStrategy, driverMatchingStrategy);
+    tripsInfo[id] = trip;
+    tripsMetaDataInfo[id] = metaData;
 }
 
-unordered_map<int, Trip*> TripMgr::getTripsMap() {
-	return tripsInfo;
+unordered_map<int, shared_ptr<Trip>> TripMgr::getTripsMap() {
+    return tripsInfo;
 }
