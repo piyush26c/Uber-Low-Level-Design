1eb47698464c71fc8fdfd1cc96ea4d1aeac08060
Piyush Chaudhari <piyushrc26@gmail.com>
2024-07-22 11:17:57 -0400

    refactored code, eliminated raw pointers and added virtual destructor

--- ratingBasedPricingStrategy.hpp
+++ ratingBasedPricingStrategy.hpp
@@ -2,12 +2,13 @@
 #include "pricingStrategy.hpp"
 #include "common.hpp"
 
-class RatingBasedPricingStrategy :public PricingStrategy {
+class RatingBasedPricingStrategy : public PricingStrategy {
 public:
-	double calculatePrice(TripMetaData* pTripMetaData) {
-		double price = Util::isHighRating(pTripMetaData->getRiderRating()) ? 55.0 : 65.0;
-		cout << "Based on " << Util::ratingToString(pTripMetaData->getRiderRating()) << 
-			" rider rating, price of the trip is " << price << endl;
-		return price;
-	}
+    double calculatePrice(const shared_ptr<TripMetaData>& pTripMetaData) const override {
+        double price = Util::isHighRating(pTripMetaData->getRiderRating()) ? 55.0 : 65.0;
+        cout << "Based on " << Util::ratingToString(pTripMetaData->getRiderRating())
+             << " rider rating, price of the trip is " << price << endl;
+        return price;
+    }
+    virtual ~RatingBasedPricingStrategy() = default; // Added virtual destructor
 };
