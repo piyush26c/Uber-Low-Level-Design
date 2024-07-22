83c01e2f4914cd015608aefe5df60c6723f6fede
Piyush Chaudhari <piyushrc26@gmail.com>
2024-07-22 11:06:29 -0400

    added smart pointers, virtual destructor (for proper clean up of derived class objects), string references for class method parameters, and refactored code little bit!

--- driverMgr.hpp
+++ driverMgr.hpp
@@ -1,16 +1,20 @@
 #pragma once
 
 #include "driver.hpp"
-#include "mutex"
+#include <mutex>
+#include <memory>
+
+using namespace std;
 
 class DriverMgr {
-	DriverMgr(){}
-	static DriverMgr* driverMgrInstance;
-	static mutex mtx;	
-	unordered_map<string, Driver*> driversMap;
+    DriverMgr() = default;  // Kept constructor private to enforce singleton pattern
+    static shared_ptr<DriverMgr> driverMgrInstance;
+    static mutex mtx;
+    unordered_map<string, shared_ptr<Driver>> driversMap;
 public:
-	static DriverMgr* getDriverMgr();
-	void addDriver(string pDriverName, Driver* pDriver);
-	Driver* getDriver(string pDriverName);
-	unordered_map<string, Driver*> getDriversMap();
+    static shared_ptr<DriverMgr> getDriverMgr();
+    void addDriver(const string& pDriverName, shared_ptr<Driver> pDriver);
+    shared_ptr<Driver> getDriver(const string& pDriverName);
+    unordered_map<string, shared_ptr<Driver>> getDriversMap();
+    virtual ~DriverMgr() = default; // Added virtual destructor
 };
