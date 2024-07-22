031aa971d681b17ca888491763f465fd90c3272d
Piyush Chaudhari <piyushrc26@gmail.com>
2024-07-22 10:59:07 -0400

    added std::move() for string constructor parameter and virtual default destructor

--- driver.hpp
+++ driver.hpp
@@ -2,20 +2,24 @@
 #include "common.hpp"
 
 class Driver {
-	string name;
-	bool avail;
-	RATING rating;
+    string name;
+    bool avail;
+    RATING rating;
 public:
-	Driver(string pName, RATING pRating) : name(pName), rating(pRating) {
-		avail = false;
-	}
-	void updateAvail(bool pAvail) {
-		avail = pAvail;
-	}
-	string getDriverName() {
-		return name;
-	}
-	RATING getRating() {
-		return rating;
-	}
+    Driver(string pName, RATING pRating) : name(std::move(pName)), rating(pRating), avail(false) {
+    }
+
+    void updateAvail(bool pAvail) {
+        avail = pAvail;
+    }
+
+    const string& getDriverName() const {
+        return name;
+    }
+
+    RATING getRating() const {
+        return rating;
+    }
+
+    virtual ~Driver() = default; // Added virtual destructor
 };
