8a70d0b94c6230ab88f23ee52a4c6f209609ba3e
Piyush Chaudhari <piyushrc26@gmail.com>
2024-07-22 11:41:48 -0400

    replaced raw pointer implementation with smart pointer and refactored code little bit.

--- uber.cpp
+++ uber.cpp
@@ -2,37 +2,32 @@
 
 int main() {
 
-	//---------------Creating Riders and Drivers--------------------------------
-	Rider* keertiRider = new Rider("Keerti", RATING::FIVE_STARS);
-	Rider* gauravRider = new Rider("Gaurav", RATING::FIVE_STARS);
-	RiderMgr* riderMgr = RiderMgr::getRiderMgr();
-	riderMgr->addRider("keerti", keertiRider);
-	riderMgr->addRider("gaurav", gauravRider);
+    // Creating Riders and Drivers
+    auto keertiRider = make_shared<Rider>("Keerti", RATING::FIVE_STARS);
+    auto gauravRider = make_shared<Rider>("Gaurav", RATING::FIVE_STARS);
+    auto riderMgr = RiderMgr::getRiderMgr();
+    riderMgr->addRider("keerti", keertiRider);
+    riderMgr->addRider("gaurav", gauravRider);
 
-	Driver* yogitaDriver = new Driver("Yogita", RATING::THREE_STARS);
-	Driver* riddhiDriver = new Driver("Riddhi", RATING::FOUR_STARS);
-	DriverMgr* driverMgr = DriverMgr::getDriverMgr();
-	driverMgr->addDriver("yogita", yogitaDriver);
-	driverMgr->addDriver("riddhi", riddhiDriver);
+    auto yogitaDriver = make_shared<Driver>("Yogita", RATING::THREE_STARS);
+    auto riddhiDriver = make_shared<Driver>("Riddhi", RATING::FOUR_STARS);
+    auto driverMgr = DriverMgr::getDriverMgr();
+    driverMgr->addDriver("yogita", yogitaDriver);
+    driverMgr->addDriver("riddhi", riddhiDriver);
 
-	//These details in turn will be stored in database
-	//-------------------------------------------------------------------------
+    auto tripMgr = TripMgr::getTripMgr();
 
-	TripMgr* tripMgr = TripMgr::getTripMgr();
+    cout << endl << "Creating Trip for Keerti from location (10,10) to (30,30)" << endl;
+    tripMgr->CreateTrip(keertiRider, make_shared<Location>(10, 10), make_shared<Location>(30, 30));
 
+    cout << endl << "Creating Trip for Gaurav from location (200,200) to (500,500)" << endl;
+    tripMgr->CreateTrip(gauravRider, make_shared<Location>(200, 200), make_shared<Location>(500, 500));
 
-	cout << endl << "Creating Trip for Keerti from location (10,10) to (30,30)" << endl;
-	tripMgr->CreateTrip(keertiRider, new Location(10, 10), new Location(30, 30));
+    // Display all the trips created
+    auto tripsMap = tripMgr->getTripsMap();
+    for (const auto& mapVal : tripsMap) {
+        mapVal.second->displayTripDetails();
+    }
 
-	cout << endl << "Creating Trip for Gaurav from location (200,200) to (500,500)" << endl;
-	tripMgr->CreateTrip(gauravRider, new Location(200, 200), new Location(500, 500));
-
-
-	//-------------------Display all the trips created--------------------------
-	unordered_map<int, Trip*> tripsMap = tripMgr->getTripsMap();
-	for (auto mapVal : tripsMap) {
-		(mapVal.second)->displayTripDetails();
-	}
-
-	return 0;
+    return 0;
 }
