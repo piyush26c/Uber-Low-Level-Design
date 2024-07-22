38f80a14857867421c036bc1f70e660408eb137c
Piyush Chaudhari <piyushrc26@gmail.com>
2024-07-22 11:28:15 -0400

    replaced mutex lock with lock_guard and same for smart pointers

--- strategyMgr.cpp
+++ strategyMgr.cpp
@@ -1,25 +1,26 @@
 #include "strategyMgr.hpp"
 
-StrategyMgr* StrategyMgr::strategyMgrInstance = nullptr;
+shared_ptr<StrategyMgr> StrategyMgr::strategyMgrInstance = nullptr;
 mutex StrategyMgr::mtx;
 
-StrategyMgr* StrategyMgr::getStrategyMgr() {
-	if (strategyMgrInstance == nullptr) {
-		mtx.lock();
-		if (strategyMgrInstance == nullptr) {
-			strategyMgrInstance = new StrategyMgr();
-		}
-		mtx.unlock();
-	}
-	return strategyMgrInstance;
+shared_ptr<StrategyMgr> StrategyMgr::getStrategyMgr() {
+    lock_guard<mutex> lock(mtx); // Used lock_guard for RAII
+    if (!strategyMgrInstance) {
+        strategyMgrInstance = shared_ptr<StrategyMgr>(new StrategyMgr());
+    }
+    return strategyMgrInstance;
 }
 
-PricingStrategy* StrategyMgr::determinePricingStrategy(TripMetaData* metaData) {
-	cout << "Based on location and other factors, setting pricing strategy" << endl;
-	return new DefaultPricingStrategy();
+shared_ptr<PricingStrategy> StrategyMgr::determinePricingStrategy(const shared_ptr<TripMetaData>& metaData) {
+    cout << "Based on location and other factors, setting pricing strategy" << endl;
+     /*no logic is being implemented to determine pricing strategy just because objective of the
+     project is to learn design patterns, OOPS, modularity and modern cpp features in C++*/
+    return make_shared<DefaultPricingStrategy>();
 }
 
-DriverMatchingStrategy* StrategyMgr::determineMatchingStrategy(TripMetaData* metaData) {
-	cout << "Based on location and other factors, setting matching strategy" << endl;
-	return new LeastTimeBasedMatchingStrategy();
+shared_ptr<DriverMatchingStrategy> StrategyMgr::determineMatchingStrategy(const shared_ptr<TripMetaData>& metaData) {
+    cout << "Based on location and other factors, setting matching strategy" << endl;
+     /*no logic is being implemented to determine driver strategy (however, we have only one driver matching strategy!)
+     just because objective of the project is to learn design patterns, OOPS, modularity and modern cpp features in C++*/
+    return make_shared<LeastTimeBasedMatchingStrategy>();
 }
